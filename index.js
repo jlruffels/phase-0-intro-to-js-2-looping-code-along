be64528be038549fae8fa56e4045479138c98005
Jessica RRuffels <jlruffels@yahoo.com>
2023-03-15 09:23:55 -0400

    looped

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+let cards=[]
+function writeCards(names) {
+    for (let i=0; i < names.length; i++) {
+    cards[i]=`Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return cards
+}
+writeCards(names)
+
+function countDown(){
+let count = 10; 
+while (count >= 0) {
+    console.log(count--)
+}
+}
